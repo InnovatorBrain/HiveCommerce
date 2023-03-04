0663c3d9a39c85985ab8bc1ebe44a962d0b83720
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-03-05 00:31:37 +0500

    first commit

---
+++ storefront/urls.py
@@ -0,0 +1,48 @@
+from django.urls import path
+from . import views
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path("", views.index, name="index"),
+path("index.html/", views.index, name="index"),
+path("about-us.html/", views.about_us, name="about-us"),
+path("blog-post.html/", views.blog_post, name="blog-post"),
+path("blog-v01.html/", views.blog_v01, name="blog-v01"),
+path("blog-v02.html/", views.blog_v02, name="blog-v02"),
+path("blog-v03.html/", views.blog_v03, name="blog-v03"),
+path("category-grid-3-cols.html/", views.category_grid_3_cols, name="category_grid_3_cols"),
+path("category-grid-left-sidebar.html/", views.category_grid_left_sidebar,name="category_grid_left_sidebar"),
+path("category-grid-right-sidebar.html/", views.category_grid_right_sidebar,name="category_grid_right_sidebar"),
+path("category-grid.html/", views.category_grid, name="category_grid"),
+path("category-list-left-sidebar.html/", views.category_list_left_sidebar,name="category_list_left_sidebar"),
+path("category-list-right-sidebar.html/", views.category_list_right_sidebar,name="category_list_right_sidebar"),
+path("category-list.html/", views.category_list, name="category_list"),
+path("checkout.html/", views.checkout, name="checkout"),
+path("contact.html/", views.contact, name="contact"),
+path("home-01.html/", views.home_01, name="home_01"),
+path("home-02.html/", views.home_02, name="home_02"),
+path("home-03.html/", views.home_03, name="home_03"),
+path("home-03-green.html", views.home_03_green, name="home-03-green.html"),
+path("home-04.html/", views.home_04, name="home_04"),
+path("home-04-light.html/", views.home_04_light, name="home-04-light.html"),
+path("home-05.html/", views.home_05, name="home_05"),
+path("contact.html/", views.contact, name="contact"),
+path("index-2.html/", views.index_2, name="index_2"),
+path("landing.html/", views.landing, name="landing"),
+path("login.html/", views.login, name="login"),
+path("shopping-cart.html/", views.shopping_cart, name="shopping_cart"),
+path("single-product-external.html/", views.single_product_external,name="single_product_external"),
+path("single-product-grouped.html/", views.single_product_grouped,name="single_product_grouped"),
+path("single-product-onsale.html/", views.single_product_onsale,name="single_product_onsale"),
+path("single-product-simple/", views.single_product_simple,name="single_product_simple"),
+path("single-product.html/", views.single_product, name="single_product"),
+path("404.html/", views.error_404, name="error_404"),
+
+]
+
+
+
+
+
---
+++ storefront/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "index.html")
+
+def about_us(request):
+    return render(request, "about-us.html")
+
+def blog_post(request):
+    return render(request, "blog-post.html")
+
+def blog_v01(request):
+    return render(request, "blog-v01.html")
+
+def blog_v02(request):
+    return render(request, "blog-v02.html")
+
+def blog_v03(request):
+    return render(request, "blog-v03.html")
+
+def category_grid_3_cols(request):
+    return render(request, "category-grid-3-cols.html")
+
+def category_grid_left_sidebar(request):
+    return render(request, "category-grid-left-sidebar.html")
+
+def category_grid_right_sidebar(request):
+    return render(request, "category-grid-right-sidebar.html")
+
+def category_grid(request):
+    return render(request, "category-grid.html")
+
+def category_list_left_sidebar(request):
+    return render(request, "category-list-left-sidebar.html")
+
+def category_list_right_sidebar(request):
+    return render(request, "category-list-right-sidebar.html")
+
+def category_list(request):
+    return render(request, "category-list.html")
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def home_01(request):
+    return render(request, "home-01.html")
+
+def home_02(request):
+    return render(request, "home-02.html")
+
+def home_03(request):
+    return render(request, "home-03.html")
+
+def home_03_green(request):
+    return render(request, "home-03-green.html")
+
+def home_04(request):
+    return render(request, "home-04.html")
+
+def home_04_light(request):
+    return render(request, "home-04-light.html")
+
+def home_05(request):
+    return render(request, "home-05.html")
+
+def index_2(request):
+    return render(request, "index-2.html")
+
+def landing(request):
+    return render(request, "landing.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def shopping_cart(request):
+    return render(request, "shopping-cart.html")
+
+def single_product_external(request):
+    return render(request, "single-product-external.html")
+
+def single_product_grouped(request):
+    return render(request, "single-product-grouped.html")
+
+def single_product_onsale(request):
+    return render(request, "single-product-onsale.html")
+
+def single_product_simple(request):
+    return render(request, "single-product-simple.html")
+
+def single_product(request):
+    return render(request, "single-product.html")
+
+def error_404(request):
+    return render(request, "404.html")
