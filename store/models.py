8c2c2ef0add27231f36e80ff71d2f6dd25840ef2
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-03-11 09:43:17 +0500

    Upload data in DataGrip according our models

--- store/models.py
+++ store/models.py
@@ -1,70 +1,63 @@
 from django.db import models
 
-# manytomany relationships between Promotion and Product
-
 
 class Promotion(models.Model):
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=255)
     discount = models.FloatField()
-    # products
-
-    # Create your models here.
-    # Collection
 
 
 class Collection(models.Model):
-    title = models.CharField(max_length=50)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null = True, related_name = '+')
-
-
-# Product
-# title, sku, description, price, inventory, last_update
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
-    sku = models.CharField(max_length=100, primary_key=True)
     title = models.CharField(max_length=255)
-    sku = models.IntegerField()
+    slug = models.SlugField(default=0)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(
-        Promotion, related_name='products')  # it shows the reverse relation
-
-
-# Customer
-# first_name, last_name, email, phone, birth_date
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=50, unique=True)
-    phone = models.CharField(max_length=50)
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-
-
-# Oder
-# placed_at, payment_status
-payment_status_pending = "P"
-payment_status_complete = "C"
-payment_status_failed = "F"
-payment_status_choice = [
-    (payment_status_pending, "Pending"),
-    (payment_status_pending, "Complete"),
-    (payment_status_pending, "Failed"),
-]
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
 class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-        max_length=1, choices=payment_status_choice, default=payment_status_pending)
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, default=1)
 
 
-# OrderItem items
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
@@ -73,26 +66,18 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
-# Relation ship of Customer address
-
 class Address(models.Model):
-    street = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    # show the relation ship with Customer class "one to one"
-    # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-    # but we want to allow for many addresses
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-
-# Cart
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
 
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
-# CartItem
-
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveBigIntegerField()
+    quantity = models.PositiveSmallIntegerField()
